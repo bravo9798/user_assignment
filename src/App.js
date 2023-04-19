4d2e966c82509f83f308bc9fe43e09d3e0d22699
bravo9798 <bhuvneshsharma9798@gmail.com>
2023-04-19 17:39:04 +0530

    removed unwanted code

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import RegistrationPage from "./pages/RegistrationPage";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import { Routes ,Route } from 'react-router-dom';
-import PrivateRoute from "./component/PrivateRoute";
 import LoginPage from "./pages/LoginPage";
 import WelcomePage from "./pages/WelcomePage";
 
 function App() {
-  // const loggedIinUser = JSON.parse(localStorage.getItem("loggedinuser"));
-  // const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  // // console.log(
-  // //   loggedIinUser.password.length > 0,
-  // //   "loggedIinUserloggedIinUserloggedIinUser"
-  // // );
-  // useEffect(() => {
-  //   if (loggedIinUser?.password.length > 0) {
-  //     setIsAuthenticated(true);
-  //   }
-  // }, []);
-
-  // console.log(isAuthenticated, "is");
 
   return (
     <div className="App">
